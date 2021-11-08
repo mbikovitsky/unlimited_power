2acf946b73b2d878a78814249f5663f75d96f607
Michael Bikovitsky <biko@bikodbg.com>
2021-11-08 18:36:09 +0200

    Make usage config params optional

--- src/bin/service/main.rs
+++ src/bin/service/main.rs
@@ -261,8 +261,8 @@ async fn ups_query_task(
     loop {
         {
             let device = HidDevice::new(
-                Some(config.hid_usage_page),
-                Some(config.hid_usage_id),
+                config.hid_usage_page,
+                config.hid_usage_id,
                 config.vendor_id,
                 config.product_id,
             )
